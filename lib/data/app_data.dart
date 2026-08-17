795f3849cc7f69973918fa398e816ab69115d1da
ling <ling@local>
2026-08-17 15:09:18 +0000

    chore: bump version to 3.0.0

--- lib/data/app_data.dart
+++ lib/data/app_data.dart
@@ -52,7 +52,7 @@ class VolumeData {
 
 /// 全局数据仓库
 class AppData {
-  static const String version = "2.9.0";
+  static const String version = "3.0.0";
 
   late Map<String, Textbook> textbooks;
   late Map<int, String> gradeNames;
