5a286aee6b3a0d8ff3b37c197b810d390194b06c
WorkBuddy <workbuddy@local>
2026-09-19 19:02:45 +0000

    chore(release): 3.4.0+30400

--- lib/data/app_data.dart
+++ lib/data/app_data.dart
@@ -52,7 +52,7 @@ class VolumeData {
 
 /// 全局数据仓库
 class AppData {
-  static const String version = "3.3.4";
+  static const String version = "3.4.0";
 
   late Map<String, Textbook> textbooks;
   late Map<int, String> gradeNames;
